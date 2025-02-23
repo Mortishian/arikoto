0d5ab33a557e93da65483778879a8d2e9b716984
NerdNextDoor <73440604+IAmTheNerdNextDoor@users.noreply.github.com>
2025-02-23 02:23:57 +0000

    Webpage time!

--- kernel/include/request.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <limine.h>
-
-// Limine requests
-extern volatile struct limine_framebuffer_request framebuffer_info;
-extern volatile struct limine_memmap_request memorymap_info;
