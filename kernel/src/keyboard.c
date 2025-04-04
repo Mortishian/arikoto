d52bf021214ab3f10d033e478ecc4c346f91d8c8
NerdNextDoor <73440604+IAmTheNerdNextDoor@users.noreply.github.com>
2025-04-04 11:07:59 +0100

    [keyboard] Fix keyboard, confirmed working on QEMU and real hardware after 3 tries in a row

--- kernel/src/keyboard.c
+++ kernel/src/keyboard.c
@@ -5,6 +5,7 @@
 #include <idt.h>
 #include <pic.h>
 #include <print.h>
+#include <pit.h>
 
 #define PS2_DATA_PORT    0x60
 #define PS2_STATUS_PORT  0x64
@@ -48,25 +49,49 @@ static void keyboard_buffer_add(char c) {
 }
 
 void init_keyboard() {
-    while (inb(0x64) & 0x2);
+    uint8_t status, response;
 
-    outb(0x64, 0xAD);
+    outb(PS2_COMMAND_PORT, 0xAD);
     io_wait();
-    outb(0x64, 0xAE);
-    io_wait();
-
-    inb(0x60);
+    outb(PS2_COMMAND_PORT, 0xA7);
     io_wait();
 
-    outb(0x64, 0x20);
+    inb(PS2_DATA_PORT);
     io_wait();
-    uint8_t config = inb(0x60);
+
+    outb(PS2_COMMAND_PORT, 0x20);
     io_wait();
-    config |= 1;
-    outb(0x64, 0x60);
+    status = inb(PS2_DATA_PORT);
     io_wait();
-    outb(0x60, config);
+    status |=1;
+    status &= ~(1 << 1);
+    outb(PS2_COMMAND_PORT, 0x60);
     io_wait();
+    outb(PS2_DATA_PORT, status);
+    io_wait();
+
+    outb(PS2_COMMAND_PORT, 0xAE);
+    io_wait();
+
+    outb(PS2_DATA_PORT, 0xFF);
+    io_wait();
+
+    int timeout = 1000;
+    while (timeout--) {
+        if ((inb(PS2_STATUS_PORT) & 1) != 0) {
+            response = inb(PS2_DATA_PORT);
+            if (response == 0xFA) break;
+        }
+        pit_sleep_ms(1);
+    }
+    outb(PS2_DATA_PORT, 0xF0);
+    io_wait();
+    outb(PS2_DATA_PORT, 0x02);
+    io_wait();
+
+    while ((inb(PS2_STATUS_PORT) & 1) != 0) {
+        inb(PS2_DATA_PORT);
+    }
 }
 
 void keyboard_callback() {
